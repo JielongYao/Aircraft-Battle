4e11f9e163f73dd3f2f9cfa23ab7e8c2265a5872
JielongYao <jyao@mun.ca>
2023-06-11 00:23:27 -0230

    .

--- src/GameWin.java
+++ src/GameWin.java
@@ -21,6 +21,15 @@ public class GameWin extends JFrame{
         //Image bgImg = Toolkit.getDefaultToolkit().getImage("imgs/bg.jpg"); //another method to access the background picture
         Image bgImg = new ImageIcon(this.getClass().getResource("imgs/bg.jpg")).getImage(); //access the background picture
         g.drawImage(bgImg, 0, 0 , null); //x = 0, y = 0
+
+        Image bossImg = new ImageIcon(this.getClass().getResource("imgs/boss.png")).getImage(); //access the boss picture
+        g.drawImage(bossImg, 190, 70 , null); //x = 190, y = 70
+
+        Image explodeImg = new ImageIcon(this.getClass().getResource("imgs/explode/e6.gif")).getImage(); //access the background picture
+        g.drawImage(explodeImg, 270, 350 , null); //x = 270, y = 350
+
+        Image planeImg = new ImageIcon(this.getClass().getResource("imgs/plane.png")).getImage(); //access the background picture
+        g.drawImage(planeImg, 280, 470 , null); //x = 280, y = 470
     }
  
 }
