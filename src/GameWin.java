584f2529d5bd00a967c82fb0b436d775382362dc
JielongYao <jyao@mun.ca>
2023-06-17 17:27:42 -0230

    .

--- src/GameWin.java
+++ src/GameWin.java
@@ -9,9 +9,9 @@ public class GameWin extends JFrame{
     int state = 0; 
 
     BgObj bgObj = new BgObj(GameUtils.bgImg, 0, -1800, 2); //background object
-    Image offScreenImage = null;
 
-    PlaneObj planeObj = new PlaneObj(GameUtils.planeImg, 37, 41, )
+    Image offScreenImage = null;
+    //PlaneObj planeObj = new PlaneObj(GameUtils.planeImg, 37, 41, )
 
     public void launch(){
 
@@ -47,48 +47,37 @@ public class GameWin extends JFrame{
 
 
     @Override
-    public void update(Graphics g){//double buffering technique to solve the screen flicker
-
-        //paint(g);
-        if (offScreenImage == null){
-            offScreenImage = createImage(600, 800);
-        }
-
-        Graphics gOff = offScreenImage.getGraphics();
-        gOff.clearRect(0, 0, 600, 800);
-        this.paint(gOff);
-        g.drawImage(offScreenImage, 0, 0, null);
-
-    }
-
-
-    @Override
     public void paint(Graphics g){
         //The paint method is called by the Event Dispatch Thread (EDT) and is basically out of my control. I never invoke paint mathod myself. 
         //When I realize a user interface such as call setVisible(true), Swing starts the EDT. This EDT thread then runs in the background and, whenever my component needs to be painted, it calls the paint method with an appropriate Graphics instance for me to use for painting.
         //For example, when window is resized, when the component is made visible, when repaint() is called... it will be called, whenever it is necessary.
+        if (offScreenImage == null){
+            offScreenImage = this.createImage(600, 800);
+        }
+        Graphics gOff = offScreenImage.getGraphics();
 
         if(state == 0){
             
             //Image bgImg = Toolkit.getDefaultToolkit().getImage("imgs/bg.jpg"); //another method to access the background picture here without GameUtils.java
             //Image bgImg = new ImageIcon(this.getClass().getResource("imgs/bg.jpg")).getImage(); //access the background picture here without GameUtils.java
-            g.drawImage(GameUtils.bgImg, 0, 0 , null); //x = 0, y = 0
+            gOff.drawImage(GameUtils.bgImg, 0, 0 , null); //x = 0, y = 0
 
-            g.drawImage(GameUtils.bossImg, 190, 70 , null); //x = 190, y = 70
+            gOff.drawImage(GameUtils.bossImg, 190, 70 , null); //x = 190, y = 70
 
-            g.drawImage(GameUtils.explodeImg, 270, 350, null); //x = 270, y = 350
+            gOff.drawImage(GameUtils.explodeImg, 270, 350, null); //x = 270, y = 350
 
-            g.drawImage(GameUtils.planeImg, 280, 470 , null); //x = 280, y = 470
+            gOff.drawImage(GameUtils.planeImg, 280, 470 , null); //x = 280, y = 470
 
             //Draw the bold italic words "Left Click to Start" on the game start interface
-            g.setColor(Color.BLUE); // set font color
-            g.setFont(new Font(Font.SANS_SERIF, Font.ITALIC + Font.BOLD, 30)); //set font name and size
-            g.drawString("Left Click to Start", 160, 320); //x = 160, y = 320
+            gOff.setColor(Color.BLUE); // set font color
+            gOff.setFont(new Font(Font.SANS_SERIF, Font.ITALIC + Font.BOLD, 30)); //set font name and size
+            gOff.drawString("Left Click to Start", 160, 320); //x = 160, y = 320
         }
 
         if(state == 1){
-            bgObj.paintSelf(g);
+            bgObj.paintSelf(gOff);
         }
+        g.drawImage(offScreenImage, 0, 0, null);
         
     }
  
